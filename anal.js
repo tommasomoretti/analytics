975bdaed93f6caedd4b35792858c07ef62127270
Tommaso Moretti <tommasomoretti88@gmail.com>
2022-10-15 15:35:39 +0200

    init

--- anal.js
+++ anal.js
@@ -1,7 +1,13 @@
 function sendData(full_endpoint, secret_key, payload, data) {
+  payload.user_agent = navigator.userAgent;
+  payload.browser = detectBrowser();
+  payload.device = detectDevice();
+  payload.platform = detectOS();
+
+
   fetch(full_endpoint, {
     // headers: new Headers({
-    //   'Authorization': 'Bearer ' + btoa('secret_key'), 
+    //   'Authorization': 'Bearer ' + btoa('secret_key'),
     //   'Content-Type': 'application/json'
     // }),
     method: 'POST',
@@ -20,6 +26,44 @@ function sendData(full_endpoint, secret_key, payload, data) {
     console.log(error)
     return data.gtmOnFailure()
   })
+}
+
+
+function detectBrowser(){
+  let userAgent = navigator.userAgent;
+  let browserName;
+
+  if(userAgent.match(/chrome|chromium|crios/i)){
+      browserName = "chrome";
+    }else if(userAgent.match(/firefox|fxios/i)){
+      browserName = "firefox";
+    }  else if(userAgent.match(/safari/i)){
+      browserName = "safari";
+    }else if(userAgent.match(/opr\//i)){
+      browserName = "opera";
+    } else if(userAgent.match(/edg/i)){
+      browserName = "edge";
+    } else{
+      browserName="unknown";
+    }
+  return browserName;
+}
+
+
+function detectDevice(){
+  let userAgent = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(userAgent)) {
+    return "Tablet";
+  }
+  else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(userAgent)) {
+    return "Mobile";
+  }
+  return "Desktop";
+}
+
+
+function detectOS(){
+  return null
 }
 
 
