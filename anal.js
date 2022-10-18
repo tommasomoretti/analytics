ff4ba58d8d713d5b4ed2d10546bd39a3746afb8a
Tommaso Moretti <tommasomoretti88@gmail.com>
2022-10-18 20:41:05 +0200

    Update anal.js

--- anal.js
+++ anal.js
@@ -66,6 +66,7 @@ function detectDevice(){
 
 // Add event listener for session end
 function sessionEndListener(full_endpoint, secret_key, data){
+  console.log("DIOCAN");
   if(getEventListeners(window).beforeunload.find(e => e.listener.name == 'sessionEnd')){
     console.log("Listener già presente")
   } else {
