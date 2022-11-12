d139bcb5c46f07ebe7af0d49ffbcd47af103846a
Tommaso Moretti <tommasomoretti88@gmail.com>
2022-11-12 11:18:40 +0100

    Update anal.js

--- anal.js
+++ anal.js
@@ -64,7 +64,7 @@ function detectDevice(){
 
 // Add event listener for page closed
 const page_closed = (full_endpoint) => {
-  return (e) => {
+  return (event) => {
     var payload = {
       event_name: 'page_closed',
       user_data: {client_id: 1234},
@@ -93,8 +93,8 @@ const page_closed = (full_endpoint) => {
 
 function addPageClosedListener(full_endpoint, mode, secret_key){    
   if (mode === 'add') { 
-    window.addEventListener("beforeunload", page_closed(full_endpoint))
+    window.addEventListener("beforeunload", page_closed(full_endpoint), true)
   } else if (mode === 'remove') {
-    window.removeEventListener("beforeunload", page_closed(full_endpoint))
+    window.removeEventListener("beforeunload", page_closed(full_endpoint), true)
   } 
 }
